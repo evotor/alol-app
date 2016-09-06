fdf82e285e5a2d55187cd6dc4789b5028cc1d5ab
Denis Mikhaylov <notxcain@gmail.com>
2016-09-06 18:39:28 +0300

    Add applyDiscount

--- client/views/card-input/code.js
+++ client/views/card-input/code.js
@@ -15,10 +15,10 @@ $(function () {
         e.preventDefault();
         loader.show();
         text.hide();
-        send(input).then(function (data) {
-            text.text(data);
+        send(input).then(function (discount) {
+            context.data.receipt.applyDiscount(discount);
             loader.hide();
-            text.show();
+            context.navigation.pushNext()
         })
     });
 
